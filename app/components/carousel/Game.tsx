4dbdb244c767a2e6a0f7f79f8f7ea9bbdef84ed2
douglasmccuen <doug@kemosabe7.com>
2020-04-25 07:44:42 -0600

    checkpoint for game

--- app/components/carousel/Game.tsx
+++ app/components/carousel/Game.tsx
@@ -1,3 +1,5 @@
+/* eslint jsx-a11y/click-events-have-key-events: off */
+/* eslint jsx-a11y/no-static-element-interactions: off */
 import React from 'react'
 import styles from './Game.css'
 
@@ -14,21 +16,25 @@ type Props = {
   isPostActive: boolean
   isPostPostActive: boolean
   isMoving: boolean
+  isSpinning: boolean
+  handleClick: () => void
 }
 
 export default function Game(props: Props) {
   const {
     name, image, description, isActive, isPreActive, isPrePreActive,
-    isPostActive, isPostPostActive, isMoving
+    isPostActive, isPostPostActive, isMoving, isSpinning, handleClick
   } = props
 
   const css = [styles.Game]
   if (isMoving) css.push(styles.isMoving)
-  if (isActive) css.push(styles.Active)
-  if (isPreActive) css.push(styles.PreActive)
-  if (isPrePreActive) css.push(styles.PrePreActive)
-  if (!isMoving && isPostActive) css.push(styles.PostActive)
-  if (isPostPostActive) css.push(styles.PostPostActive)
+  if (!isSpinning) {
+    if (isActive) css.push(styles.Active)
+    if (isPreActive) css.push(styles.PreActive)
+    if (isPrePreActive) css.push(styles.PrePreActive)
+    if (!isMoving && isPostActive) css.push(styles.PostActive)
+    if (isPostPostActive) css.push(styles.PostPostActive)
+  }
 
   const style = {
     backgroundImage: `url(${image})`
@@ -36,7 +42,7 @@ export default function Game(props: Props) {
 
   return (
     <div className={css.join(' ')}>
-      <div className={styles.Image} style={style} />
+      <div className={styles.Image} style={style} onClick={handleClick} />
       <div className={styles.Name}>{ name }</div>
       <div className={styles.Desc}>{ description }</div>
     </div>
