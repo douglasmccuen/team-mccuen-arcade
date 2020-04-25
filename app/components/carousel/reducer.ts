e2b2a1831a857af266cf1c44ee00f49e4042e778
douglasmccuen <doug@kemosabe7.com>
2020-04-25 07:48:30 -0600

    checkpoint for reducer

--- app/components/carousel/reducer.ts
+++ app/components/carousel/reducer.ts
@@ -1,25 +1,42 @@
 import { CarouselAction, CarouselState } from './types'
 import { previous, next } from './helpers'
+import { transitionTime } from './constants'
 
 const carouselReducer = (state: CarouselState, action: CarouselAction): CarouselState => {
   switch (action.type) {
+    case 'jump':
+      return {
+        ...state,
+        desired: action.desired,
+        transitionTime
+      };
     case 'next':
       return {
         ...state,
         desired: next(action.length, state.active),
         pause: action.pause||1,
+        transitionTime
       };
     case 'prev':
       return {
         ...state,
         desired: previous(action.length, state.active),
         pause: action.pause||1,
+        transitionTime
+      };
+    case 'spin':
+      return {
+        ...state,
+        spinCount: action.rotations,
+        spinning: true
       };
     case 'done':
       return {
         ...state,
         offset: NaN,
         active: state.desired,
+        spinning: false,
+        spinCount: 0
       };
     default:
       return state;
