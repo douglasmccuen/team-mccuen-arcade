2b63c6c67e0531008461b8c17678e1bed77e35f5
douglasmccuen <doug@kemosabe7.com>
2020-04-26 15:23:11 -0600

    note

--- app/components/carousel/sound/index.tsx
+++ app/components/carousel/sound/index.tsx
@@ -4,6 +4,7 @@ import clickSound from './roulettewheel.mp3'
 
 const carouselClick = new UIFx(clickSound, { volume: 0.4,  throttleMs: 0 })
 
+// Not sure this works in production... see prespin for a different impl
 const Sound = () => {
   carouselClick.play()
   return (<></>)
