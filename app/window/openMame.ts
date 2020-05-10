994a5041d14b57ae8bed4aa98eaa51a7d7c245ab
douglasmccuen <doug@kemosabe7.com>
2020-05-09 19:48:58 -0600

    open app stuff

--- app/window/openMame.ts
+++ app/window/openMame.ts
@@ -4,14 +4,17 @@ import getConfig from '../config'
 
 const openMame = ({game, config}:EmulatorProps, cb:Callback):ChildProcess => {
   const {mamePath, mameExec} = config
-  const cmd = `cd ${mamePath}; ./${mameExec} ${game.game}`
+
+  // windows vs mac differences follow...
+  const cmd = `cd ${mamePath}; ${mameExec} ${game.game}`
+
   const options = {}
   return exec(cmd, options, cb)
 }
 
 export const openMameProcess = async (cb:Callback):Promise<ChildProcess> => {
   const process = await getConfig().then(({mameExec, mamePath}) => {
-    const cmd = `cd ${mamePath}; ./${mameExec} `
+    const cmd = `cd ${mamePath}; ${mameExec} `
     const options = {}
     return exec(cmd, options, cb)
   })
--- app/window/openRetroArch.ts
+++ app/window/openRetroArch.ts
@@ -4,17 +4,19 @@ import getConfig from '../config'
 
 const openRetroArch = ({game, config}:EmulatorProps, cb:Callback):ChildProcess => {
   const {romConfigPath, retroArchPath, retroArchExec} = config
-  const cd = `cd ~/${retroArchPath}`
-  const core = `./Resources/cores/${game.core}`
-  const rom = `~/${romConfigPath}/${game.game}`
-  const cmd = `${cd}; ./${retroArchExec} -f -L ${core} ${rom}`
+
+  // windows vs mac differences follow...
+  const cd = `cd ${retroArchPath}`
+  const core = `Resources\\cores\\${game.core}`
+  const rom = `${romConfigPath}\\${game.game}`
+  const cmd = `${cd}; ${retroArchExec} -f -L ${core} ${rom}`
   const options = {}
   return exec(cmd, options, cb)
 }
 
 export const openRetroArchProcess = async (cb:Callback):Promise<ChildProcess> => {
   const process = await getConfig().then(({retroArchPath, retroArchExec}) => {
-    const cmd = `cd ${retroArchPath}; ./${retroArchExec} -f`
+    const cmd = `cd ${retroArchPath}; ${retroArchExec} -f`
     const options = {}
     return exec(cmd, options, cb)
   })
